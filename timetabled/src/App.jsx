997d699fff6a291316da876dd10f7d68d7f4119a
Briar Rose <briarjrose@gmail.com>
2026-04-17 03:43:23 -0400

    New files

---
+++ timetabled/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import Calendar from './components/Calendar'
+
+const App = () => {
+
+  return (
+    <div className="App">
+      <h1>Goonie's Trip Around The World</h1>
+      <h2>One dog with a single mission: To Explore!</h2>
+      <Calendar />
+    </div>
+  )
+}
+
+export default App
---
+++ timetabled/src/components/Calendar.jsx
@@ -0,0 +1,134 @@
+import Event from "./Event";
+
+const Calendar = () => {
+  return (
+    <div className="Calendar">
+      <table>
+        <thead>
+          <tr>
+            <th></th>
+            <th>Sunday</th>
+            <th>Monday</th>
+            <th>Tuesday</th>
+            <th>Wednesday</th>
+            <th>Thursday</th>
+            <th>Friday</th>
+            <th>Saturday</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td className="time">8 am</td>
+            <Event
+              event="Fancy Dinner 🎩"
+              color="green"
+              location="Red Robins"
+            />
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td className="time">9 am</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <Event
+              event="Scuba Diving!"
+              color="blue"
+              location="The Deep Blue Sea"
+            />
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td className="time">10 am</td>
+            <td></td>
+            <Event event="Fencing!" color="yellow" location="France" />
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td className="time">11 am</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td className="time">12 pm</td>
+            <td></td>
+            <td></td>
+            <Event event="Being a Dog!" color="pink" location="Home" />
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td className="time">1 pm</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td className="time">2 pm</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td className="time">3 pm</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td className="time">4 pm</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td className="time">5 pm</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default Calendar;
